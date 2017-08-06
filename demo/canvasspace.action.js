07a51e014b49953c1fdca22350d3a85db4210d4c
William Ngan <williamngan@users.noreply.github.com>
2017-08-06 02:29:25 -0700

    demos in progress

--- demo/canvasspace.action.js
+++ demo/canvasspace.action.js
@@ -1,16 +1,35 @@
+window.demoDescription = "A set of points records the mouse trail as the mouse moves. When mouse is down and dragging, the trail will extend. When released, the trail gradually shortens.";
+
 (function() {
 
   Pts.namespace( this );
-
-  var space = new CanvasSpace("#pt").setup({retina: true});
+  var space = new CanvasSpace("#pt").setup({bgcolor: "#fe3", resize: true, retina: true});
   var form = space.getForm();
 
+
+  //// Demo code ---
+
+  var chain = new Group();
+  var stretch = false;
+
   space.add({ 
+    animate: (time, ftime) => {
+      if (chain.length > ((stretch) ? 100 : 10)) chain.shift();
+
+      form.strokeOnly("#123", 3).line( chain );
+      form.fillOnly("#123").point( space.pointer, 10, "circle")
+    },
+
     action:( type, px, py ) => {
-      form.stroke("rgba(0,0,0,.2)").line( [space.center, [px, py]] );
+      if (type == "down") stretch = true;
+      if (type == "up") stretch = false; 
+      if (type == "move") chain.push( new Pt(px, py) );  
     } 
   });
+  
+  //// ----
+  
 
-  space.bindMouse();
+  space.bindMouse().bindTouch().play();
 
 })();
---
+++ demo/canvasspace.resize.js
@@ -0,0 +1,35 @@
+window.demoDescription = "Draw shapes based on the size of space. Resize the window and the drawing will update.";
+
+(function() {
+
+  Pts.namespace( this );
+
+  var space = new CanvasSpace("#pt").setup({bgcolor: "#96bfed", resize: true, retina: true});
+  var form = space.getForm();
+
+
+  //// Demo code ---
+
+  var currBound = new Bound();
+
+  space.add({ 
+    animate: (time, ftime) => {
+      let quads = Rectangle.quadrants( [new Pt(), currBound.size] );
+      let circles = quads.map( (q) => Rectangle.toCircle( q ) );
+      
+      form.fillOnly("#09f").circles( circles );
+      form.strokeOnly( "#fff", 2).lines( quads );
+      form.fill("#f03").point( space.center, 10, "circle" );
+      form.log( "Size: "+currBound.size.toString() );
+    },
+
+    resize: (bound, evt) => { currBound = bound; }
+    
+  });
+
+  //// ----
+  
+
+  space.bindMouse().bindTouch().play();
+
+})();
