cdaad211caaacba12f3703cb3f3d952d17f1019f
William Ngan <williamngan@users.noreply.github.com>
2017-09-21 20:26:39 -0700

    minor fix on demo

--- demo/htmlform.scope.js
+++ demo/htmlform.scope.js
@@ -3,7 +3,7 @@ window.demoDescription = "Pts also provides experimental support for rendering H
 (function() {
   
   Pts.namespace( this );
-  var space = new HTMLSpace("#pt").setup({bgcolor: "#000", resize: true });
+  var space = new HTMLSpace("#pt").setup({bgcolor: "#36f", resize: true });
   var form = space.getForm();
   
 
@@ -13,7 +13,7 @@ window.demoDescription = "Pts also provides experimental support for rendering H
   // css for testing
   var css = document.createElement("style");
   css.type = "text/css";
-  css.innerHTML = ".pts-rect { font-size: 30vw; line-height: 1; overflow: hidden; } .r1, .r2 { opacity: 0.9 }";
+  css.innerHTML = ".pts-rect { font-size: 30vw; line-height: 1; overflow: hidden; } .r2 { opacity: 0.9 } .r1a { border-bottom-color: #fff !important; }";
   document.body.appendChild(css);
 
 
@@ -26,16 +26,18 @@ window.demoDescription = "Pts also provides experimental support for rendering H
 
       let p = space.pointer.$max(0, 0).$min( space.size );
       let r1 = Rectangle.fromTopLeft( [0,0], space.size );
-      let r1Alt = Rectangle.fromTopLeft( [0,0], [p.x, p.y] );
+      let r1Alt = Rectangle.fromTopLeft( [10,0], [p.x, p.y] );
       let r2 = Rectangle.fromTopLeft( [p.x, 0], space.size );
 
-      form.fillOnly("#36f").fillText("#fe3").cls("rect1").rect( r1 );
-      form.cls("rect1a").fillText("#fff").stroke("#fff", 1).rect( r1Alt );
-      document.querySelector(".rect1").textContent = "hello";
-      document.querySelector(".rect1a").textContent = "hello";
+      // Draw first rectangle(s)
+      form.strokeOnly("#36f", 1).fillText("#fe3").cls("r1").rect( r1 );
+      form.strokeOnly("#36f", 1).fillText("#fff").cls("r1a").rect( r1Alt );
+      document.querySelector(".r1").textContent = "hello";
+      document.querySelector(".r1a").textContent = "hello";
 
-      form.fillOnly("#f03").fillText("#fff").cls("r2 rect2").rect( r2 );
-      document.querySelector(".rect2").textContent = "world";
+      // Draw second rectange
+      form.fillOnly("#f03").fillText("#fff").cls("r2").rect( r2 );
+      document.querySelector(".r2").textContent = "world";
     }
   );
 
