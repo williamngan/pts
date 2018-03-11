a32c000c9dfdc8f6809007336fe2e4d2c258c1e1
William Ngan <williamngan@users.noreply.github.com>
2018-03-11 03:13:24 -0700

    fix npm module

---
+++ src/_module.ts
@@ -0,0 +1,32 @@
+import * as _Bound from "./Bound";
+import * as _Canvas from "./Canvas";
+import * as _Create from "./Create";
+import * as _Form from "./Form";
+import * as _LinearAlgebra from "./LinearAlgebra";
+import * as _Num from "./Num";
+import * as _Op from "./Op";
+import * as _Pt from "./Pt";
+import * as _Space from "./Space";
+import * as _Color from "./Color";
+import * as _Util from "./Util";
+import * as _Dom from "./Dom";
+import * as _Svg from "./Svg";
+import * as _Typography from "./Typography";
+
+
+module.exports = {
+  ..._Bound,
+  ..._Canvas,
+  ..._Create,
+  ..._Form,
+  ..._LinearAlgebra,
+  ..._Op,
+  ..._Num,
+  ..._Pt,
+  ..._Space,
+  ..._Util,
+  ..._Color,
+  ..._Dom,
+  ..._Svg,
+  ..._Typography
+};
---
+++ webpack.mod.js
@@ -0,0 +1,45 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var LIB_PATH = path.resolve(__dirname, 'src/_module.ts')
+var BUILD_PATH = path.resolve(__dirname, 'dist')
+
+module.exports = {
+  
+  entry: {
+    pts: LIB_PATH
+  },
+
+  output: {
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    path: BUILD_PATH,
+    filename: 'index.js'
+  },
+
+  // devtool: 'source-map',
+
+  // watchOptions: { poll: true }, // seems to need this for Windows Linux subsystem to watch
+
+  resolve: {
+    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+  },
+
+  module: {
+    rules:[
+      {
+        test: /\.tsx?$/, loader: "tslint-loader", enforce: "pre",
+        options: {emitErrors: true, failOnHint: true}
+      },
+      { 
+        test: /\.tsx?$/, 
+        loader: 'ts-loader'
+      }
+    ],
+  },
+
+  plugins: [
+    new webpack.BannerPlugin("pts.js - Copyright © 2017-2018 William Ngan and contributors.\nLicensed under Apache 2.0 License.\nSee https://github.com/williamngan/pts for details.")
+  ]
+
+};
