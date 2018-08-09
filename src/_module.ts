ca9abeed4c20df0891a5e9217b9094223e3c94ef
Ben Southgate <bsouthga@gmail.com>
2018-08-09 17:07:58 -0400

    fix: only commit PR related changes

--- src/_module.ts
+++ src/_module.ts
@@ -1,32 +1,16 @@
-import * as _Canvas from "./Canvas";
-import * as _Create from "./Create";
-import * as _Form from "./Form";
-import * as _LinearAlgebra from "./LinearAlgebra";
-import * as _Num from "./Num";
-import * as _Op from "./Op";
-import * as _Pt from "./Pt";
-import * as _Space from "./Space";
-import * as _Color from "./Color";
-import * as _Util from "./Util";
-import * as _Dom from "./Dom";
-import * as _Svg from "./Svg";
-import * as _Typography from "./Typography";
-import * as _Physics from "./Physics";
+export * from "./Canvas";
+export * from "./Create";
+export * from "./Form";
+export * from "./LinearAlgebra";
+export * from "./Num";
+export * from "./Op";
+export * from "./Pt";
+export * from "./Space";
+export * from "./Color";
+export * from "./Util";
+export * from "./Dom";
+export * from "./Svg";
+export * from "./Typography";
+export * from "./Physics";
 
 
-module.exports = {
-  ..._Canvas,
-  ..._Create,
-  ..._Form,
-  ..._LinearAlgebra,
-  ..._Op,
-  ..._Num,
-  ..._Pt,
-  ..._Space,
-  ..._Util,
-  ..._Color,
-  ..._Dom,
-  ..._Svg,
-  ..._Typography,
-  ..._Physics,
-};
