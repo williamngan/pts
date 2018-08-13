09c637891400483a169c83c007666afc34ee9dca
William Ngan <williamngan@users.noreply.github.com>
2018-08-13 00:48:08 -0700

    Fix negative index error

--- src/Create.ts
+++ src/Create.ts
@@ -238,8 +238,8 @@ export class Noise extends Pt {
    */
   noise2D() {
   
-    let i = Math.floor( this._n[0] ) % 255;
-    let j = Math.floor( this._n[1] ) % 255;
+    let i = Math.max(0, Math.floor( this._n[0] )) % 255;
+    let j = Math.max(0, Math.floor( this._n[1] )) % 255;
     let x = (this._n[0] % 255) - i;
     let y = (this._n[1] % 255) - j;
 
