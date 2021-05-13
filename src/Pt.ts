e1c9ab0be3196e5bbfd1c7095bfc57a24b2518b4
Carlo Colombo <carlo.colombo@gmx.com>
2021-05-13 21:26:47 +0200

    Replace Math.random with Num.random

--- src/Pt.ts
+++ src/Pt.ts
@@ -39,7 +39,7 @@ export class Pt extends Float32Array implements IPt, Iterable<number> {
     if (defaultValue) p.fill( defaultValue );
     if (randomize) {
       for (let i=0, len=p.length; i<len; i++) {
-        p[i] = p[i]*Math.random();
+        p[i] = p[i]*Num.random();
       }
     }
     return new Pt( p );
